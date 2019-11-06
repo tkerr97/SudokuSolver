bbfc17825b668d11acce5401fe851fef2ba4433e
tkerr <tkerr@mymail.mines.edu>
2019-11-06 14:34:01 -0700

    added reading image and converting to grayscale

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -3,9 +3,19 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <string>
 
 int main()
 {
-	
+	std::cout << "Image File: ";
+	std::string filename;
+	std::cin >> filename;
+	cv::Mat image = cv::imread(filename);
+	if (!image.data) {
+		std::cout << "Could not open the file!" << std::endl;
+		return -1;
+	}
+	cv::Mat grayImage;
+	cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+
 }
